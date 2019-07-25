e5002ee5486564a511ded4a56d48756796d8af64
Oku-Dan <oku_dan411@yahoo.co.jp>
2019-07-26 01:43:22 +0900

    first commit

---
+++ OpenCVtest/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace OpenCVtest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startstop = new System.Windows.Forms.Button();
+            this.isSync = new System.Windows.Forms.CheckBox();
+            this.distance = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.distance)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startstop
+            // 
+            this.startstop.Location = new System.Drawing.Point(13, 13);
+            this.startstop.Name = "startstop";
+            this.startstop.Size = new System.Drawing.Size(75, 23);
+            this.startstop.TabIndex = 0;
+            this.startstop.Text = "開始";
+            this.startstop.UseVisualStyleBackColor = true;
+            this.startstop.Click += new System.EventHandler(this.startstop_Click);
+            // 
+            // isSync
+            // 
+            this.isSync.AutoSize = true;
+            this.isSync.Checked = true;
+            this.isSync.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.isSync.Location = new System.Drawing.Point(95, 13);
+            this.isSync.Name = "isSync";
+            this.isSync.Size = new System.Drawing.Size(89, 19);
+            this.isSync.TabIndex = 1;
+            this.isSync.Text = "同期実行";
+            this.isSync.UseVisualStyleBackColor = true;
+            // 
+            // distance
+            // 
+            this.distance.Location = new System.Drawing.Point(13, 43);
+            this.distance.Name = "distance";
+            this.distance.Size = new System.Drawing.Size(120, 22);
+            this.distance.TabIndex = 2;
+            this.distance.Value = new decimal(new int[] {
+            60,
+            0,
+            0,
+            0});
+            this.distance.ValueChanged += new System.EventHandler(this.distance_ValueChanged);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.distance);
+            this.Controls.Add(this.isSync);
+            this.Controls.Add(this.startstop);
+            this.Name = "MainForm";
+            this.Text = "OpenCVtest";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.distance)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startstop;
+        private System.Windows.Forms.CheckBox isSync;
+        private System.Windows.Forms.NumericUpDown distance;
+    }
+}
+
---
+++ OpenCVtest/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace OpenCVtest
+{
+    public partial class MainForm : Form
+    {
+        private VideoCapture cap;
+        private bool capturing = false,islastSeted = false;
+        private Mat lastDescriptor = new Mat(),lastBuffer;
+        private Point lastPoint;
+        private KeyPoint[] lastkeyPoints;
+        private float distanceStandard = 60;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void CaptureLoop()
+        {
+            while (capturing)
+            {
+                await Task.Run(() =>
+                {
+                    Mat img = new Mat();
+                    GetImg(ref img);
+                    Cv2.ImShow("CapturedImage", img);
+                    Cv2.WaitKey(1);
+                    img.Release();
+                    img = null;
+                });
+            }
+            cap.Release();
+            Cv2.DestroyAllWindows();
+        }
+
+        private void CaptureLoopSync()
+        {
+            Mat img = new Mat();
+            while (capturing)
+            {
+                GetImg(ref img);
+                Cv2.ImShow("CapturedImage", img);
+                Cv2.WaitKey(100);
+            }
+            img.Release();
+            img = null;
+            cap.Release();
+            Cv2.DestroyAllWindows();
+            islastSeted = false;
+        }
+
+        private void GetImg(ref Mat buffer)
+        {
+            cap.Read(buffer);
+            buffer = Process(ref buffer);
+            return;
+        }
+
+        private void distance_ValueChanged(object sender, EventArgs e)
+        {
+            distanceStandard = (float)distance.Value * 10.0F;
+        }
+
+        private Mat Process(ref Mat buffer)
+        {
+            Mat img = new Mat();
+            AKAZE akaze = AKAZE.Create();
+            akaze.Threshold = 0.0001;
+            KeyPoint[] keyPoints;
+            DMatch[] matches;
+            List<DMatch> goodMatches = new List<DMatch>();
+            Mat descriptor = new Mat();
+            DescriptorMatcher matcher = DescriptorMatcher.Create("BruteForce");
+            Cv2.CvtColor(buffer, buffer, ColorConversionCodes.BGR2GRAY);
+            akaze.DetectAndCompute(buffer, null, out keyPoints, descriptor);
+            Cv2.DrawKeypoints(buffer, keyPoints,img,Scalar.Black);
+            Cv2.ImShow("keyps", img);
+            if (islastSeted)
+            {
+                matches = matcher.Match(descriptor, lastDescriptor);
+                for (int i = 0; i < matches.Length; i++)
+                    if (matches[i].Distance < distanceStandard)
+                        goodMatches.Add(matches[i]);
+                //Cv2.DrawMatches(buffer, keyPoints, lastBuffer, lastkeyPoints, goodMatches, img);
+                img = buffer;
+                if (goodMatches.Count > 3)
+                {
+                    float[] average = new float[2];
+                    average[0] = 0; average[1] = 0;
+                    for (int i = 0; i < goodMatches.Count; i++)
+                    {
+                        average[0] += keyPoints[goodMatches[0].QueryIdx].Pt.X -
+                        lastkeyPoints[goodMatches[0].TrainIdx].Pt.X;
+                        average[1] += keyPoints[goodMatches[0].QueryIdx].Pt.Y -
+                        lastkeyPoints[goodMatches[0].TrainIdx].Pt.Y;
+                    }
+                    lastPoint = new Point(lastPoint.X + average[0] / goodMatches.Count, lastPoint.Y + average[1] / goodMatches.Count);
+                    lastBuffer = buffer;
+                    lastDescriptor = descriptor;
+                    lastkeyPoints = keyPoints;
+                }
+                Cv2.Circle(img, lastPoint, 15, Scalar.Red, 3);
+            }
+            else
+            {
+                islastSeted = true;
+                img = buffer;
+                lastPoint = new Point(buffer.Cols / 2, buffer.Rows / 2);
+                lastBuffer = buffer;
+                lastDescriptor = descriptor;
+                lastkeyPoints = keyPoints;
+            }
+            
+            return img;
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (capturing) capturing = false;
+        }
+
+        private void startstop_Click(object sender, EventArgs e)
+        {
+            if (capturing)
+            {
+                startstop.Text = "開始";
+                capturing = false;
+            }
+            else
+            {
+                startstop.Text = "停止";
+                capturing = true;
+                cap = new VideoCapture(0);
+                if (!cap.IsOpened()) return;
+                if(isSync.Checked)CaptureLoopSync();
+                else CaptureLoop();
+            }
+        }
+    }
+}
